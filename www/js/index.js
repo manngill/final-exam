572fef612e846c9f8ae10de59f51cad2d6ef7bd8
Manpreet kaur <manpreetgill1946@gmail.com>
2018-11-01 09:15:42 -0400

    finalexam

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,124 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+document.addEventListener("deviceReady", connectToDatabase);
+document.getElementById("insert").addEventListener("click",  heroes inserted);
+document.getElementById("show").addEventListener("click", show heroes);
+document.getElementById("rescue").addEventListener("click", rescue me);
+
+var db = null;
+
+
+function connectToDatabase() {
+  console.log("device is ready - connecting to database");
+
+  if (window.cordova.platformId === 'browser') {
+    console.log("browser detected...");
+
+    db = window.openDatabase("superdb", "1.0", "Database for superdb app", 5*1024*1024);
+  }
+  else {
+    alert("mobile device detected");
+    console.log("mobile device detected!");
+
+    var databaseDetails = {"name":"superdb.db", "location":"default"}
+    db = window.sqlitePlugin.openDatabase(databaseDetails);
+    console.log("done opening db");
+  }
+
+  if (!db) {
+    alert("databse not opened!");
+    return false;
+  }
+
+  db.transaction(
+    function(tx){
+
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS Columns (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, isAvailable INTEGER)",
+        [],
+        onSuccessExecuteSql,
+        onError
+      )
     },
+    onError,
+    onReadyTransaction
+  );
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function insertHeroes() {
 
-        console.log('Received Event: ' + id);
+  console.log("Heroes is inserted!");
+  alert("insert button pressed!");
+
+  var i = document.getElementById("idBox").value;
+  var n = document.getElementById("nameBox").value;
+    var a = document.getElementById("availableBox").value;
+
+  console.log("id: " + i);
+  console.log("name: " + n);
+    console.log("isAvailable " + a);
+
+  db.transaction(
+        function(tx){
+            tx.executeSql( "INSERT INTO Columns(id, name, isAvailable) VALUES(?,?,?)",
+            [i,n,a],
+            onSuccessExecuteSql,
+            onError )
+        },
+        onError,
+        onReadyTransaction
+    )
+
+
+}
+
+function showHeroes() {
+
+  console.log("show button pressed!");
+  alert("show button pressed!");
+
+  db.transaction(
+        function(tx){
+            tx.executeSql( "SELECT * FROM Columns",
+            [],
+            displayResults,
+            onError )
+        },
+        onError,
+        onReadyTransaction
+    )
+}
+
+function displayResults( tx, results ){
+
+    if(results.rows.length == 0) {
+            alert("No records found");
+            return false;
+        }
+
+        var row = "";
+        for(var i=0; i<results.rows.length; i++) {
+      document.getElementById("resultsSection").innerHTML +=
+          "<p> ID: "
+        +   results.rows.item(i).id
+        + "<br>"
+        + "Name: "
+        +   results.rows.item(i).name
+        +"<br>"
+        + "isAvailable "
+          +   results.rows.item(i).isAvailable
+        + "</p>";
+
+        }
+
     }
-};
 
-app.initialize();
+function onReadyTransaction( ){
+  console.log( 'Transaction completed' )
+}
+function onSuccessExecuteSql( tx, results ){
+  console.log( 'Execute SQL completed' )
+}
+function onError( err ){
+  console.log( err )
+}
